a234e04e81d1217c23bfce83d90cebe2bd6cf95f
Narek <narek@35mm.am>
2018-08-12 14:49:21 +0400

    adding Probe Model

---
+++ models/probe.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProbeSchema = new Schema ({
+    
+    probeURL:{
+        type:String,
+        required:true,
+    },
+
+    interval:{
+        type:Number,
+        required:true,
+        default:60
+    },
+
+    method:{
+        type:String,
+        default:'GET',
+    },
+
+    port:{
+        type:Number,
+        required:true,
+        default:80
+    },
+    
+    timeout: {
+		type: Number
+    },
+    
+	matchPolicy: {
+		exactMatch: {
+			type: Boolean
+		},
+		keywords: {
+			type: [
+				String
+			]
+		}
+	},
+    
+    body: {
+		type: String
+    },
+    
+	auth: {},
+    
+    json: {
+		type: Boolean
+	},
+    
+    headers: {
+        
+    },
+
+    notification_policy_id : {
+        type: Schema.Types.ObjectId,
+        ref:'NotificationPolicy'
+     },
+
+     user_id:{
+        type: Schema.Types.ObjectId,
+        ref:'User'
+     },
+
+});
+
+const Probe = mongoose.model('Probe',ProbeSchema);
+
+module.exports = Probe;
