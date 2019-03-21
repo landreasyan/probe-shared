a449c9bff5456a074407d4fa1b3a69d4b5cb907c
Narek Kozmoyan <narek@Lanas-MacBook-Pro-2.local>
2019-03-21 11:24:54 +0400

    new model for notification-log/history

---
+++ models/notification-log.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var NotificationLogSchema = new Schema({
+    
+    logTime: {
+        type: Date, 
+        default: Date.now
+    },
+
+    channel: {
+        type: String
+    },
+
+    channelType: {
+        type: String
+    },
+
+    channel_id:{
+        type: Schema.Types.ObjectId,
+        ref:'Notification-Channel'
+    },
+
+    subject:{
+        type: String
+    },
+
+    user_id:{
+        type: Schema.Types.ObjectId,
+        ref:'User'
+     },
+
+    probe_id:{
+        type: Schema.Types.ObjectId,
+        ref:'Probe'
+    }
+});
+
+
+module.exports = mongoose.model('Notification-Log', NotificationLogSchema);
